3a506dd00bca82b9406a59bca1a380ca81c96778
Viktor Abrahamsson <vikabr0524@gmail.com>
2025-02-09 17:13:32 +0100

    initial commit

---
+++ src/test/java/com.viktorstaskmanager/MainTest.java
@@ -0,0 +1,11 @@
+package com.viktorstaskmanager;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+  @Test
+  public void testSample() {
+    assertTrue(true);  // Simple test to check the setup
+  }
+}
