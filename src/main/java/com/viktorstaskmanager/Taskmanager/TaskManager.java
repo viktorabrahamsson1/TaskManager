7a03da93eb2734a4f6687f0954fb3ca98621a6aa
Viktor Abrahamsson <vikabr0524@gmail.com>
2025-02-11 11:55:23 +0100

    refactored and finished user logic

--- src/main/java/com/viktorstaskmanager/Taskmanager/TaskManager.java
+++ src/main/java/com/viktorstaskmanager/Taskmanager/TaskManager.java
@@ -1,16 +1,92 @@
 package com.viktorstaskmanager.Taskmanager;
 
+import com.viktorstaskmanager.factorys.UserFactory;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TaskManager {
-  private ArrayList<User> users;
+  private final ArrayList<User> users;
+  private final Scanner scanner;
+  private User loggedInUser = null;
 
-  public TaskManager(){
+  public TaskManager() {
     this.users = new ArrayList<>();
+    scanner = new Scanner(System.in);
   }
 
-  public void addUser(User user){
-    users.add(user);
+  public void logIn() {
+    if (this.loggedInUser != null) {
+      String name = scanner.nextLine();
+      String password = scanner.nextLine();
+      User user = UserFactory.logIn(name, password, users);
+
+      if (user != null) {
+        System.out.println("Successfully logged in.");
+      } else {
+        System.out.println("User not found.");
+      }
+      this.loggedInUser = user;
+    } else {
+      System.out.println("You are already logged in.");
+    }
   }
+
+  public void logOut() {
+    if (loggedInUser != null) {
+      loggedInUser = UserFactory.logOut();
+    } else {
+      System.out.println("You are already logged out.");
+    }
+  }
+
+  public void createUser() {
+    String name;
+    String password;
+    String confirmPassword;
+
+    do {
+      System.out.print("Enter your name: ");
+      name = scanner.nextLine().trim();
+
+      if (name.isEmpty()) {
+        System.out.println("Your name cant be empty.");
+      }
+
+    } while (name.isEmpty());
+
+    do {
+      System.out.print("Enter your password: ");
+      password = scanner.nextLine();
+      System.out.print("Confirm your password: ");
+      confirmPassword = scanner.nextLine();
+
+      if (!password.equals(confirmPassword)) {
+        System.out.println("Passwords do not math, try again!");
+      }
+    } while (!password.equals(confirmPassword));
+
+    if (UserFactory.addUserToList(name, password, users)) {
+      System.out.println("User created successfully");
+    } else {
+      System.out.println("Failed to create user.");
+    }
+
+  }
+
+  public void deleteUser() {
+    System.out.print("Enter the name to delete: ");
+    String name = scanner.nextLine();
+    System.out.print("Enter the password to confirm delete: ");
+    String password = scanner.nextLine();
+
+    if (UserFactory.removeUserFromList(name, password, this.loggedInUser, this.users)) {
+      System.out.println("User successfully removed.");
+      this.logOut();
+    } else {
+      System.out.println("Failed to remove user.");
+    }
+  }
+
 
 }
--- src/main/java/com/viktorstaskmanager/factorys/UserFactory.java
+++ src/main/java/com/viktorstaskmanager/factorys/UserFactory.java
@@ -3,71 +3,45 @@ package com.viktorstaskmanager.factorys;
 import com.viktorstaskmanager.Taskmanager.User;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class UserFactory {
   private UserFactory() {
     //Prevents instantiation
   }
 
-  public static User createUser(Scanner scanner) {
-    String name;
-    String password;
-    String confirmPassword;
+  public static boolean addUserToList(String name, String password, ArrayList<User> users) {
+    if (name == null || password == null || name.isEmpty() || password.isEmpty()) {
+      return false;
+    }
 
-    do {
-      System.out.print("Enter your name: ");
-      name = scanner.nextLine().trim();
-
-      if (name.isEmpty()) {
-        System.out.println("Your name cant be empty.");
-      }
-
-    } while (name.isEmpty());
-
-    do {
-      System.out.print("Enter your password: ");
-      password = scanner.nextLine();
-      System.out.print("Confirm your password: ");
-      confirmPassword = scanner.nextLine();
-
-      if (!password.equals(confirmPassword)) {
-        System.out.println("Passwords do not math, try again!");
-      }
-    } while (!password.equals(confirmPassword));
-
-
-    return new User(name, password);
-  }
-
-  public static User deleteUser(Scanner scanner, User loggedInUser) {
-    System.out.print("Enter the name to delete: ");
-    String name = scanner.nextLine();
-    System.out.print("Enter the password to confirm delete: ");
-    String password = scanner.nextLine();
-
-    if (name.equals(loggedInUser.getName()) && password.equals(loggedInUser.getPassword())) {
-      System.out.println("User successfully deleted.");
-      return loggedInUser;
-    } else {
-      System.out.println("User not found.");
-      return null;
+    try {
+      users.add(new User(name, password));
+      return true;
+    } catch (Exception e) {
+      return false;
     }
   }
 
-  public static User logIn(Scanner scanner, ArrayList<User> users) {
-    String name = scanner.nextLine();
-    String password = scanner.nextLine();
+  public static boolean removeUserFromList(String name, String password, User loggedInUser, ArrayList<User> users) {
+    for (User user : users) {
+      if (name.equals(user.getName()) && password.equals(user.getPassword()) && user.equals(loggedInUser)) {
+        users.remove(user);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static User logIn(String name, String password, ArrayList<User> users) {
     for (User user : users) {
       if (name.equals(user.getName()) && password.equals(user.getPassword())) {
         return user;
       }
     }
-    System.out.println("User not found.");
     return null;
   }
 
-  public static void logOut() {
-
+  public static User logOut() {
+    return null;
   }
 }
